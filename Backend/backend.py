725a2d327e1067314aeb2b2f042516d69a3a2234
Exile404 <tahsinul.haque.dhrubo@g.bracu.ac.bd>
2023-06-20 00:19:21 +0600

    v0.0.2

--- Backend/backend.py
+++ Backend/backend.py
@@ -15,6 +15,7 @@ class Amazon_Tracker(webdriver.Chrome):
         self.implicitly_wait(300)
         self.maximize_window()
         self.main_list = []
+        self.store = {'name':[],'price':[]}
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
@@ -22,3 +23,18 @@ class Amazon_Tracker(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def search_and_select(self,item):
+        search_field = self.find_element(By.ID,'twotabsearchtextbox')
+        search_field.clear()
+        search_field.send_keys(item)
+        search_click = self.find_element(By.ID,'nav-search-submit-button')
+        search_click.click()
+        item_lists = self.find_elements(By.CSS_SELECTOR,'span.a-size-medium.a-color-base.a-text-normal')
+        price_list = self.find_elements(By.CSS_SELECTOR,'span.a-price-whole')
+
+        for item in item_lists:
+            self.store['name'].append(item.text)
+        for price in price_list:
+            self.store['price'].append(price.text)
+        print(self.store)
